7ecf6e450a21d69339049bf8775aa5c0bf7e78dc
hasnat.jamil <hasnatjamil2018@gmail.com>
2024-02-18 11:24:46 +0600

    changeset: update logger file name, fix import in actions

--- src/core/actions.core.ts
+++ src/core/actions.core.ts
@@ -1,14 +1,16 @@
 import { Page } from '@playwright/test'
-import { logger } from '../utils/logger'
+import { logger } from '../utils/logger.util'
 import { allure } from 'allure-playwright'
 import { screenShotsPath } from '../config/test.config'
 import { randomUUID } from 'crypto'
 
 export class ActionsCore {
   protected page: Page
+
   constructor(page: Page) {
     this.page = page
   }
+
   protected async executeAction<T>(
     description: string,
     action: () => Promise<T>
@@ -18,7 +20,7 @@ export class ActionsCore {
       await allure.step(description, async () => await action())
     } catch (error) {
       logger.error(
-        `error while executing the step: ${description}\nerror message: ${error}`
+        `error while executing the step: ${description}\nerror message: ${error.message}`
       )
       const screenshot = await this.page.screenshot({
         path: `${screenShotsPath}/${randomUUID()}.png`,
